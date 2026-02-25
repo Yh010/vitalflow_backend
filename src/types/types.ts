86c5aec4b49d507e3bb5add3e593eb880a28f864
AdityaRaj-007 <iamadityaraj.2001@gmail.com>
2026-02-25 21:50:35 +0530

    Chat history endpoint.

--- src/types/types.ts
+++ src/types/types.ts
@@ -5,3 +5,21 @@
 // }
 
 // export let items: Item[] = [];
+
+export type ChatMessage = {
+  role: "user" | "model";
+  parts: { text: string }[];
+};
+
+export type TextToSpeechLanguage =
+  | "bn-IN"
+  | "en-IN"
+  | "gu-IN"
+  | "hi-IN"
+  | "kn-IN"
+  | "ml-IN"
+  | "mr-IN"
+  | "od-IN"
+  | "pa-IN"
+  | "ta-IN"
+  | "te-IN";
